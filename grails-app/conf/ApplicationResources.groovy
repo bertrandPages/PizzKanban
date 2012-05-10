73a840ba3c9f18ae5cd3d87264fd4740b4823bcf
Silvere Lestang <silvere.lestang@gmail.com>
2012-05-10 21:56:44 +0200

    Add backbone.js

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,13 @@
 modules = {
     application {
+        dependsOn 'jquery, underscore, backbone'
+
         resource url:'js/application.js'
+    }
+    backbone {
+        resource url: '/js/backbone.js'
+    }
+    underscore {
+        resource url: '/js/underscore-1.1.6.js'
     }
 }
